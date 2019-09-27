f325689b1be85160cdae4c6897d29d6e69b3ec7b
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:45:55 -0500

    Create Grupos.cs

---
+++ CLS/Grupos.cs
@@ -0,0 +1,8 @@
+class Grupos  
+{  
+    public string id_grupo { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
