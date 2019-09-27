437fd3f45fd59a3683935fad990bb889fa9ba36a
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:50:02 -0500

    Create Semestre.cs

---
+++ CLS/Semestre.cs
@@ -0,0 +1,8 @@
+class Semestre  
+{  
+    public string id_semestre { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
