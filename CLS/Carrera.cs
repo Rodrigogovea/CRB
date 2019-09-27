c322d436643fc8b5e4fe8b7561f1bc087423c612
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:40:12 -0500

    Create Carrera.cs

---
+++ CLS/Carrera.cs
@@ -0,0 +1,8 @@
+class Carrera  
+{  
+    public string id_carrera { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
