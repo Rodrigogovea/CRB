37cf38fd123c269b6648e91d590d7dd09c7085d6
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:39:14 -0500

    Create AlumnosDetalle.cs

---
+++ CLS/AlumnosDetalle.cs
@@ -0,0 +1,12 @@
+class Alumnos_detalle  
+{  
+    public string No_Control { get; set; }  
+    public string id_periodo { get; set; }  
+    public string id_semestre { get; set; }  
+    public string id_grupo { get; set; }  
+    public string id_turno { get; set; }  
+    public string id_carrera { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
