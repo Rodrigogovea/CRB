919e503953aee52d2fdf2efeebc039e9e1557a35
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:48:04 -0500

    Create Recargas.cs

---
+++ CLS/Recargas.cs
@@ -0,0 +1,9 @@
+class Recargas  
+{  
+    public string Folio_recarga { get; set; }  
+    public string No_Control { get; set; }  
+    public decimal monto { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
