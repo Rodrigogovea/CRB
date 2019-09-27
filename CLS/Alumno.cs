37426a5aa04ff80beef010d333c0ac281f326962
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:38:13 -0500

    Create Alumno.cs

---
+++ CLS/Alumno.cs
@@ -0,0 +1,16 @@
+class Alummno  
+{  
+    public string No_Control { get; set; }  
+    public string Nombre { get; set; }  
+    public string curp { get; set; }  
+    public string nacionalidad { get; set; }  
+    public DateTime fecha_nacimiento { get; set; }  
+    public string sexo { get; set; }  
+    public string Tel_movil { get; set; }  
+    public string Tel_particular { get; set; }  
+    public string Correo { get; set; }  
+    public decimal saldo_impresion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
