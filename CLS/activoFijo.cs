dfcefb0b91f9c029a36a93c3084cf48a1ee4cea0
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:36:55 -0500

    clase activoFijo
    clase activoFijo

---
+++ CLS/activoFijo.cs
@@ -0,0 +1,11 @@
+class Activofijo  
+{  
+    public string id_activoFijo { get; set; }  
+    public string id_equipos { get; set; }  
+    public string id_centro { get; set; }  
+    public string id_lugar { get; set; }  
+    public string id_situacion { get; set; }  
+    public int estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
