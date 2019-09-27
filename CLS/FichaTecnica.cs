ed191a9283b4aaacdd7b328717984514aa6c1952
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:42:43 -0500

    Create FichaTecnica.cs

---
+++ CLS/FichaTecnica.cs
@@ -0,0 +1,9 @@
+class Ficha_tecnica  
+{  
+    public string id_activoFijo { get; set; }  
+    public string idAtributo { get; set; }  
+    public string Descripcion { get; set; }  
+    public int estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
