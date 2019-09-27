0d72931f8342e484de1697877485151b62fea762
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:51:23 -0500

    Create Turno.cs

---
+++ CLS/Turno.cs
@@ -0,0 +1,8 @@
+class Turno  
+{  
+    public string id_turno { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
