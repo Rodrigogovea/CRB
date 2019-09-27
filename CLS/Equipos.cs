c2c69e19cd2279f903b9731e82ef1a0e42b5c00b
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:41:43 -0500

    Create Equipos.cs

---
+++ CLS/Equipos.cs
@@ -0,0 +1,8 @@
+class Equipos  
+{  
+    public string id_equipos { get; set; }  
+    public string Descripcion { get; set; }  
+    public int estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
