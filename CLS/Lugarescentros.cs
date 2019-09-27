66ce9ff1c9a868a9f0ff267ff5fa69b9f7108f7b
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:47:22 -0500

    Create Lugarescentros.cs

---
+++ CLS/Lugarescentros.cs
@@ -0,0 +1,8 @@
+class Lugarescentros  
+{  
+    public string id_centro { get; set; }  
+    public string id_lugar { get; set; }  
+    public int estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
