95ad0864d3eca1c4031809e9bd319f40c3edaa8a
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:51:03 -0500

    Create Tipoimpresiones.cs

---
+++ CLS/Tipoimpresiones.cs
@@ -0,0 +1,9 @@
+class Tipoimpresiones  
+{  
+    public string id_tipo_impresiones { get; set; }  
+    public string Descripcion { get; set; }  
+    public decimal precio { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
