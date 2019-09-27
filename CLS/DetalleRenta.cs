9e9b4839108922523ac3367fbecd4231c85035bc
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:41:04 -0500

    Create DetalleRenta.cs

---
+++ CLS/DetalleRenta.cs
@@ -0,0 +1,10 @@
+class Detalle_renta  
+{  
+    public string id_renta { get; set; }  
+    public int Consecutivo { get; set; }  
+    public string id_incidente { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
