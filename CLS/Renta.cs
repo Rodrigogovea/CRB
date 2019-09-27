9567493f5ee0dea83b5de57829f1ecb5d739d3d9
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:48:34 -0500

    Create Renta.cs

---
+++ CLS/Renta.cs
@@ -0,0 +1,13 @@
+class Renta  
+{  
+    public string id_renta { get; set; }  
+    public string id_activoFijo { get; set; }  
+    public string No_Control { get; set; }  
+    public string id_periodo { get; set; }  
+    public string Descripcion { get; set; }  
+    public DateTime horaInicio { get; set; }  
+    public DateTime horaFin { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
