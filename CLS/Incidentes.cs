dc5702d9571720788b5fc1f8d86e43ca13b5615a
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:46:55 -0500

    Create Incidentes.cs

---
+++ CLS/Incidentes.cs
@@ -0,0 +1,8 @@
+class Incidentes  
+{  
+    public string id_incidente { get; set; }  
+    public string Descripcion { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
