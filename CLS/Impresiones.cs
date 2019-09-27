f2aeaecb4a273ea200ed335baa97cb7c5c9227ee
totto8a <55634312+totto8a@users.noreply.github.com>
2019-09-27 10:46:32 -0500

    Create Impresiones.cs

---
+++ CLS/Impresiones.cs
@@ -0,0 +1,11 @@
+class Impresiones  
+{  
+    public string id_impresiones { get; set; }  
+    public string Descripcion { get; set; }  
+    public string No_Control { get; set; }  
+    public string id_tipo_impresiones { get; set; }  
+    public int cantidad_impresa { get; set; }  
+    public string estado { get; set; }  
+    public DateTime fechaMov { get; set; }  
+    public string id_usuario_modifica { get; set; }  
+}
