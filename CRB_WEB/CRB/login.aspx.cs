08d66a38fb9a21e5d4d21315aaece6976a883ea6
Totto8a <poncho.totto8@gmail.com>
2019-09-27 22:11:21 -0500

    login medio terminado

---
+++ CRB_WEB/CRB/login.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+
+namespace CRB
+{
+
+    public partial class Login : System.Web.UI.Page
+    {
+        
+    }
+}
